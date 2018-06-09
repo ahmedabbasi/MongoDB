efc6af4995c5618fe40a8c193d7acb43acd43ec5
Ahmed Abbasi <aabbasi@ncstech.com>
2018-06-09 16:59:33 -0400

    new

--- server.js
+++ server.js
@@ -25,10 +25,16 @@ console.log(db);
 
 var PORT = 3000;
 
-var databaseUri = "mongodb://localhost/scraper";
+//var databaseUri = "mongodb://localhost/scraper";
 //
-mongoose.connect(databaseUri)
+//mongoose.connect(databaseUri)
 
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
